8cbd65542e095ad08d45fcf44573d8c28af7b391
tmdwns950428 <tmdwns950428@naver.com>
2023-01-30 01:08:58 +0900

    redux-thunk

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SampleContainer from './containers/SampleContainer';
+// import CounterContainers from './containers/CounterContainers';
+
+function App() {
+  return (
+    <div>
+      {/* <CounterContainers/> */}
+      {<SampleContainer/>}
+    </div>
+  );
+}
+
+export default App;
